ba3934293c7e2ac86fc9379a9900f639515d91b5
Kamisama12 <458654817@qq.com>
2024-06-27 23:00:08 +0800

    新增loader

---
+++ loader/read_gdp.py
@@ -0,0 +1,21 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# =====================================================
+# @File   : read_gdp.py
+# @Date   : 2024/6/27 21:31
+# @Author : yuzhaohao
+#
+# 2024/02/01 1:14   Create
+# =====================================================
+
+
+from loader.xlsx_loader import XlsxLoader
+
+def read_dpg(filepath):
+    loader = XlsxLoader(filepath)
+    result=loader.load()
+    result=''.join(result)
+    print(result)
+
+if __name__ == '__main__':
+    read_dpg(r"../tests/data/GDP.xlsx")
